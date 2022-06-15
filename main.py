42da7304a452f0c54fa98467ea9b137ea9ff2195
Lisandro <lisandro@progresstech.dev>
2022-06-14 23:48:38 -0300

    check the weird message

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from flask import Flask
 from flask import request
 from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = '/data/'
+ALLOWED_EXTENSIONS = {'json'}
+
 # Inputs
 # csv: datetime (1/1/19 0:00), eload (kWh), Temp (F)
 # Baseline start date - Baseline end date
@@ -29,17 +32,18 @@ app = Flask(__name__)
 def upload_data():
     if request.method == 'POST':
         f = request.files['the_file']
-        f.save('./data/')
+        f.save(UPLOAD_FOLDER)
+    return 'File uploaded'
+
 
 def main():
     # meter_data, temperature_data, sample_metadata = (
     #     eemeter.load_sample("il-electricity-cdd-hdd-hourly")
     # )
     #
-    # print(sample_metadata)
+    print("high")
     # meter_data.head()
 
 
 if __name__ == '__main__':
     main()
-
