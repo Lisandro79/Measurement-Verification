aee882eb3ca949bfc87bc91fc7241d491068a906
Lisandro <lisandro@progresstech.dev>
2022-07-27 17:27:59 -0300

    add baseline_fit to the response

--- main.py
+++ main.py
@@ -19,5 +19,6 @@ def receive_data():
 
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
+    # serve(app, host='0.0.0.0', port=8080)
 
